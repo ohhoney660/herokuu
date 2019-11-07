1d00672d892a328f2f3bea86435c54d627de8b4f
swetha-aggidevara <swetha@ilimi.in>
2019-11-07 15:22:38 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 // server.js
 const express = require('express');
+const path = require('path');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + './dist/herokuu'));
 // Start the app by listening on the default
 // Heroku port
+app.get('/', (req, res) => {
+    res.send(path.join(__dirname, '/dist/herokuuindex.html'))
+})
 app.listen(process.env.PORT || 8080, () => {
-    console.log('app started at port 8080');
+    console.log('app started at port:', process.env.PORT );
 });
